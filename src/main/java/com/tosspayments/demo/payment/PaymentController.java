cb27c5d1bf16d874c412e7a385a95fc6ba4a46ad
ohyo555 <555gywn@gamil.com>
2024-04-09 15:07:57 +0900

    API key 등록

--- src/main/java/com/tosspayments/demo/payment/PaymentController.java
+++ src/main/java/com/tosspayments/demo/payment/PaymentController.java
@@ -26,7 +26,7 @@ public class PaymentController {
             @RequestParam(value = "amount") Integer amount,
             @RequestParam(value = "paymentKey") String paymentKey) throws Exception {
 
-        String secretKey = "test_sk_PBal2vxj81NdvOpzplvJ35RQgOAN";
+        String secretKey = "test_sk_PBal2vxj81NdvOpzplvJ35RQgOAN:";
 
         Base64.Encoder encoder = Base64.getEncoder();
         byte[] encodedBytes = encoder.encode(secretKey.getBytes("UTF-8"));
